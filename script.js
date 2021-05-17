4e387427d601442fe596e3d0c88e5c8a52d0d29e
Antonio Melena <melenaantonio@gmail.com>
2021-05-17 15:36:33 -0500

    added grey black and rainbow buttons and added hover to change divs to grey automatically

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 let buttonsContainer = document.querySelector('.buttons');
+
+// creating all buttons 
 const resetButton = document.createElement('button');
 const buttonText = document.createTextNode('Reset');
 resetButton.appendChild(buttonText);
-document.body.appendChild(resetButton);
+buttonsContainer.appendChild(resetButton);
+
+const greyButton = document.createElement('button');
+const greyButtonText = document.createTextNode('Grey');
+greyButton.appendChild(greyButtonText);
+buttonsContainer.appendChild(greyButton);
+
+const blackButton = document.createElement('button');
+const blackButtonText = document.createTextNode('Black');
+blackButton.appendChild(blackButtonText);
+buttonsContainer.appendChild(blackButton);
+
+const rainbowButton = document.createElement('button');
+const rainbowButtonText = document.createTextNode('Rainbow');
+rainbowButton.appendChild(rainbowButtonText);
+buttonsContainer.appendChild(rainbowButton);
+
 const container = document.getElementById("container");
 
 function createGrid(col, row){
    for (i = 0; i < (col * row); i++){
-      let div = document.createElement('div');
+      const div = document.createElement('div');
       container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
       container.style.gridTemplateRows = `repeat(${row}, 1fr)`
       container.appendChild(div);
@@ -15,8 +33,15 @@ function createGrid(col, row){
    }
 }
 
+createGrid(16, 16);
+
 function resetPage(){
    window.location.reload();
 }
 
 resetButton.addEventListener('click', resetPage);
+
+const divX = container.querySelectorAll(".square");
+divX.forEach(div => div.addEventListener('mouseover', function(){
+   div.classList.add("grey");
+}));
