1a8777a35171af2715dda10f099dd36ff991715d
Antonio Melena <melenaantonio@gmail.com>
2021-08-07 01:26:58 -0500

    changed background image

--- script.js
+++ script.js
@@ -1,89 +1,88 @@
-let buttonsContainer = document.querySelector('.buttons');
+let buttonsContainer = document.querySelector(".buttons");
 const container = document.querySelector(".container");
 
-// creating all buttons 
-const resetButton = document.createElement('button');
-const buttonText = document.createTextNode('Change Size');
+// creating all buttons
+const resetButton = document.createElement("button");
+const buttonText = document.createTextNode("Change Size");
 resetButton.appendChild(buttonText);
 buttonsContainer.appendChild(resetButton);
 
-const greyButton = document.createElement('button');
-const greyButtonText = document.createTextNode('Grey');
+const greyButton = document.createElement("button");
+const greyButtonText = document.createTextNode("Grey");
 greyButton.appendChild(greyButtonText);
 buttonsContainer.appendChild(greyButton);
 
-const blackButton = document.createElement('button');
-const blackButtonText = document.createTextNode('Black');
+const blackButton = document.createElement("button");
+const blackButtonText = document.createTextNode("Black");
 blackButton.appendChild(blackButtonText);
 buttonsContainer.appendChild(blackButton);
 
-const rainbowButton = document.createElement('button');
-const rainbowButtonText = document.createTextNode('Rainbow');
+const rainbowButton = document.createElement("button");
+const rainbowButtonText = document.createTextNode("Rainbow");
 rainbowButton.appendChild(rainbowButtonText);
 buttonsContainer.appendChild(rainbowButton);
 
-
-function createGrid(col, row){
-   for (i = 0; i < (col * row); i++){
-      const div = document.createElement('div');
-      container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
-      container.style.gridTemplateRows = `repeat(${row}, 1fr)`
-      container.appendChild(div);
-      div.classList.add("square");
-   }
+function createGrid(col, row) {
+  for (i = 0; i < col * row; i++) {
+    const div = document.createElement("div");
+    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+    container.appendChild(div);
+    div.classList.add("square");
+  }
 }
 
 createGrid(16, 16);
 
-function getNewGridSize(){
-   let size = prompt("Enter A New Size For The Grid (1 - 100)");
-   if (size <= 100 && size > 0) {
-      return size;
-   } else if (size < 1 || size > 100){
-      resetPage();
-   } 
+function getNewGridSize() {
+  let size = prompt("Enter A New Size For The Grid (1 - 100)");
+  if (size <= 100 && size > 0) {
+    return size;
+  } else if (size < 1 || size > 100) {
+    resetPage();
+  }
 }
 
-function removeAllChildNodes(parent){
-   while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-   }
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
 
-function resetPage(){
-   removeAllChildNodes(container);
-   let size = getNewGridSize();
-   createGrid(size, size);
+function resetPage() {
+  removeAllChildNodes(container);
+  let size = getNewGridSize();
+  createGrid(size, size);
 }
 
-resetButton.addEventListener('click', resetPage);
+resetButton.addEventListener("click", resetPage);
 
 greyButton.addEventListener("click", function () {
-   const divX = container.querySelectorAll(".square");
-   divX.forEach((div) =>
-      div.addEventListener("mouseover", () => {
-         div.style.backgroundColor = `rgb(169, 169, 169)`;
-      })
-   );
+  const divX = container.querySelectorAll(".square");
+  divX.forEach((div) =>
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = `rgb(169, 169, 169)`;
+    })
+  );
 });
 
 blackButton.addEventListener("click", function () {
-   const divX = container.querySelectorAll(".square");
-   divX.forEach((div) =>
-      div.addEventListener("mouseover", () => {
-         div.style.backgroundColor = `rgb(0, 0, 0)`;
-      })
-   );
+  const divX = container.querySelectorAll(".square");
+  divX.forEach((div) =>
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = `rgb(0, 0, 0)`;
+    })
+  );
 });
 
 rainbowButton.addEventListener("click", function () {
-   const divX = container.querySelectorAll(".square");
-   divX.forEach((div) =>
-      div.addEventListener("mouseover", () => {
-         let numberR = Math.floor(Math.random() * 256);
-         let numberG = Math.floor(Math.random() * 256);
-         let numberB = Math.floor(Math.random() * 256);
-         div.style.backgroundColor = `rgb(${numberR}, ${numberG}, ${numberB})`;
-      })
-   );
+  const divX = container.querySelectorAll(".square");
+  divX.forEach((div) =>
+    div.addEventListener("mouseover", () => {
+      let numberR = Math.floor(Math.random() * 256);
+      let numberG = Math.floor(Math.random() * 256);
+      let numberB = Math.floor(Math.random() * 256);
+      div.style.backgroundColor = `rgb(${numberR}, ${numberG}, ${numberB})`;
+    })
+  );
 });
